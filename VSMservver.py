a9e131d22d232c483dbdbdefae516b4fded1ea31
Mervin24 <mdalmet4@gmail.com>
2018-04-05 09:53:10 +0530

    removed route

--- VSMservver.py
+++ VSMservver.py
@@ -19,6 +19,78 @@ cursor=conn.cursor()
 
 #INSERT INTO `users` (`username`, `password`, `phone_number`, `email`, `name`, `id_num`) VALUES ('Mervin24', 'deep1BC', '8974561230', 'mdalmet@gmail.com', 'Mervin Dalmet', NULL);
 
+
+
+
+	
+def getcsv():
+	cursor=conn.cursor()
+	
+	cursor.execute("SELECT * FROM `company` ;")
+	result=cursor.fetchall()
+	print(type(result))
+	for i in result :
+		name = "/static/data/"+i[0]+".csv"
+		f = open(name,'r')
+		list = f.readlines()
+		f.close()
+		f = open(name,'w')
+		if list==[]:
+			element = "0,"+str(i[3])
+			f.write(str(element))
+			#print("e")
+		else:
+			count = len(list)
+			new_element = "\n"+str(count)+","+str(i[3])
+			list.append(new_element)
+			for i in list:
+				f.write(str(i))
+			print(count)
+		f.close()
+		print(i)
+	for i in result :	
+		cursor.execute("UPDATE company SET Price = '"+str(int(i[3]+i[3]*rand.uniform(-0.01,0.01)))+"' WHERE CompanyId = '"+str(i[0])+"'")
+		print ("changed"+str(i[3])+" "+str(int(i[3]+i[3]*rand.uniform(-0.01,0.01))))
+		conn.commit()
+	return 'Tr'
+	
+def plot():
+	cursor=conn.cursor()
+	cursor.execute("SELECT * FROM `company` ;")
+	result=cursor.fetchall()
+	print(type(result))
+	count = 0
+	x = []
+	y = []
+	for i in result :
+		print(count)
+		count+=1
+		name = "/static/data/"+i[0]+".csv"
+		
+		x.clear()
+		y.clear()
+		with open(name, 'r') as csvfile:
+			plots= csv.reader(csvfile, delimiter=',')
+			for row in plots:
+				x.append(int(row[0]))
+				y.append(int(row[1]))	
+		print(len(y))		
+		plt.plot(x, y, color='g')
+		plt.xlabel('Days')
+		plt.ylabel('Price')
+		
+		plt.title('Stock Value')
+		png_name = "/static/images/"+i[0]+".png"
+		plt.savefig(png_name)
+		plt.gcf().clear()
+			
+	return 'plotted'
+
+
+
+
+
+
 @app.route("/")
 def index():
 	return render_template('login.html')
@@ -91,71 +163,7 @@ def stylesheet():
 	return render_template("myHoldingcssfile.css")
 
 
-	
-@app.route("/getcsv",methods=['GET', 'POST'])	
-def getcsv():
-	cursor=conn.cursor()
-	
-	cursor.execute("SELECT * FROM `company` ;")
-	result=cursor.fetchall()
-	print(type(result))
-	for i in result :
-		name = "/static/data/"+i[0]+".csv"
-		f = open(name,'r')
-		list = f.readlines()
-		f.close()
-		f = open(name,'w')
-		if list==[]:
-			element = "0,"+str(i[3])
-			f.write(str(element))
-			#print("e")
-		else:
-			count = len(list)
-			new_element = "\n"+str(count)+","+str(i[3])
-			list.append(new_element)
-			for i in list:
-				f.write(str(i))
-			print(count)
-		f.close()
-		print(i)
-	for i in result :	
-		cursor.execute("UPDATE company SET Price = '"+str(int(i[3]+i[3]*rand.uniform(-0.01,0.01)))+"' WHERE CompanyId = '"+str(i[0])+"'")
-		print ("changed"+str(i[3])+" "+str(int(i[3]+i[3]*rand.uniform(-0.01,0.01))))
-		conn.commit()
-	return 'Tr'
-	
-@app.route("/plot",methods=['GET', 'POST'])	
-def plot():
-	cursor=conn.cursor()
-	cursor.execute("SELECT * FROM `company` ;")
-	result=cursor.fetchall()
-	print(type(result))
-	count = 0
-	x = []
-	y = []
-	for i in result :
-		print(count)
-		count+=1
-		name = "/static/data/"+i[0]+".csv"
-		
-		x.clear()
-		y.clear()
-		with open(name, 'r') as csvfile:
-			plots= csv.reader(csvfile, delimiter=',')
-			for row in plots:
-				x.append(int(row[0]))
-				y.append(int(row[1]))	
-		print(len(y))		
-		plt.plot(x, y, color='g')
-		plt.xlabel('Days')
-		plt.ylabel('Price')
-		
-		plt.title('Stock Value')
-		png_name = "/static/images/"+i[0]+".png"
-		plt.savefig(png_name)
-		plt.gcf().clear()
-			
-	return 'plotted'
+
 	
 if __name__ == "__main__":
 	app.run(host="0.0.0.0",port=5050,debug=True)
